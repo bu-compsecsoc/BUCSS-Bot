e59c3b9686168841274fd4f31b5e7aa97217d6b0
James <james@jamesmcc.co.uk>
2023-01-19 17:28:56 +0000

    removed redundant code from index

--- index.js
+++ index.js
@@ -39,32 +39,5 @@ for (const file of eventFiles) {
 	}
 }
 
-/*
-// On Ready
-client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag}!`);
-})
-
-
-// On Slash Command
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-    console.log(interaction);
-
-    const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-
-})*/
 
 client.login(token);

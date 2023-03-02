9533fa05285bdd6949f53410fe4d8d25595573b8
Ben Brady <benbradybusiness@gmail.com>
2023-03-02 23:55:32 +0000

    Removed Ping

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,12 +1,10 @@
 import activity from "./activity";
-import ping from "./ping";
 import status from "./status";
 import verifyMember from "./verifyMember";
 import writeAnnouncement from "./writeAnnouncement";
 
 export const commands = [
     activity,
-    ping,
     status,
     verifyMember,
     writeAnnouncement

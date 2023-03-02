347aa38bb7db5fac61917038baed975cd12130b8
James <james@jamesmcc.co.uk>
2023-03-02 17:34:39 +0000

    Completed the membership verify command

---
+++ commands/verifyMember.js
@@ -0,0 +1,55 @@
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle} = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('membership')
+		.setDescription('Sends your account for a membership review!')
+        .addStringOption(option =>
+            option.setName('studentid')
+                .setDescription('Your student ID - (With the "s" prefix)')
+                .setRequired(true)
+        ),
+	async execute(interaction) {
+        // Check if user has the member role
+        usersRoles = interaction.member.roles.member._roles
+        for (const role of usersRoles) {
+            if (process.env.MEMBERID == role) {
+                await interaction.reply({
+                    content: ':rotating_light: You are already a member!',
+                    ephemeral: true
+                });
+                return;
+            }
+        }
+
+        const id = interaction.options.getString('studentid');
+        const studentIdRegEx = new RegExp('s[0-9]{7}');
+        if (!studentIdRegEx.test(id)) {
+            await interaction.reply({
+                content: ':rotating_light: Invalid student ID! Make sure it starts with the "s"',
+                ephemeral: true
+            });
+            return;
+        }
+
+        // ID is valid, send for review
+        const channel = interaction.client.channels.cache.get(process.env.MEMBERREVIEWCHANNEL);
+        userID = interaction.member.user;
+
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId(`memberVerify|${userID.id}`)
+                    .setLabel('Yes, this is a member')
+                    .setStyle(ButtonStyle.Success),
+            )
+
+        channel.send({ content: `================\nCheck ${userID} against ${id}`, components: [row]});
+
+    
+		await interaction.reply({
+            content: `:white_check_mark: Your account (${id}) has been sent for a membership review!`,
+            ephemeral: true
+        });
+	},
+};
--- events/buttonInteraction.js
+++ events/buttonInteraction.js
@@ -27,5 +27,19 @@ module.exports = {
 
             await interaction.reply({ content: ":rotating_light:" + italic(bold("THIS IS A PREVIEW MESSAGE")) + ":rotating_light:\n\n" + template.createAnnoucement(announceData), ephemeral: false, components: [row] });
         }
+
+        // Member Verification Button
+        if (interaction.customId.startsWith('memberVerify')) {
+            const role = interaction.guild.roles.cache.get(process.env.MEMBERID)
+            const member = role.guild.members.cache.get(interaction.customId.split('|')[1])
+            member.roles.add(role);
+            member.createDM().then(channel => {
+                channel.send({ content: "Congratulations, you have been Verified as a member of BUCSS!" });
+            }).catch(error => {
+                console.error(error);
+                interaction.reply({ content: "An error occured! Please DM an admin", ephemeral: true });
+            });
+            await interaction.reply({ content: `Added Role to ${member}!` , ephemeral: true });
+        }
 	},
 };

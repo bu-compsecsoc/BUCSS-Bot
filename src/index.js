b3708234aeee0b3b1ea9c96ba19b07d03321a3e7
Ben Brady <benbradybusiness@gmail.com>
2023-03-03 01:03:45 +0000

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,17 @@
 require('dotenv').config()
 // Require Discord.JS
-const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
 const { commands } = require('./commands');
 const { events } = require('./events');
 const { deployCommands } = require("./deployCommands");
 const token = process.env.TOKEN;
 
-// Create Client
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// Create Collection
+
 client.commands = new Collection();
-
-
 for (const command of commands) {
-    // Set a new item in the Collection with the key as the command name and the value as the exported module
-    if ("data" in command && "execute" in command) {
-        client.commands.set(command.data.name, command);
-    } else {
-        console.log(
-            `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
-        );
-    }
+    client.commands.set(command.data.name, command);
 }
 
 for (const event of events) {
